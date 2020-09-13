1537ab0b11a5b32bf007572be07f61aeddb5b3f5
Your_Name <joseph161216@gmail.com>
2020-09-13 01:22:27 -0700

    add bootstrap styles to login and sign up pages

--- src/components/SignupPage/SignupPage.jsx
+++ src/components/SignupPage/SignupPage.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 import * as userActions from "../../actions/UserActions";
 
@@ -43,85 +45,87 @@ const SignupPage = (props) => {
 
   return (
     <div>
-      <h2>Sign Up</h2>
-      <form name="form" onSubmit={handleSubmit}>
-        <div
-          className={
-            "form-group" + (submitted && !username ? " has-error" : "")
-          }
-        >
-          <label htmlFor="email">Username</label>
-          <input
-            type="text"
-            // className="form-control"
-            name="username"
-            value={username}
-            onChange={handleChange}
-          />
-          {submitted && !username && (
-            <div className="help-block">Username is required</div>
-          )}
-        </div>
-        <div
-          className={
-            "form-group" + (submitted && !email ? " has-error" : "")
-          }
-        >
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            // className="form-control"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-          {submitted && !email && (
-            <div className="help-block">Email is required</div>
-          )}
-        </div>
-        <div
-          className={
-            "form-group" + (submitted && !password ? " has-error" : "")
-          }
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            // className="form-control"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-          {submitted && !password && (
-            <div className="help-block">Password is required</div>
-          )}
-        </div>
-        <div
-          className={
-            "form-group" +
-            (submitted && !password_confirmation ? " has-error" : "")
-          }
-        >
-          <label htmlFor="password">Confirm Password</label>
-          <input
-            type="password"
-            // className="form-control"
-            name="password_confirmation"
-            value={password_confirmation}
-            onChange={handleChange}
-          />
-          {submitted && !password_confirmation && (
-            <div className="help-block">Password Confirmation is required</div>
-          )}
-        </div>
-        <div className="form-group">
-          <button>Sign Up</button>
-          {loggingIn && (
-            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-          )}
-        </div>
-      </form>
-      <Link to="/login">Login</Link>
+      <Container>
+        <h2>Sign Up</h2>
+        <form name="form" onSubmit={handleSubmit}>
+          <div
+            className={
+              "form-group" + (submitted && !username ? " has-error" : "")
+            }
+          >
+            <label htmlFor="email">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
+            {submitted && !username && (
+              <div className="help-block">Username is required</div>
+            )}
+          </div>
+          <div
+            className={"form-group" + (submitted && !email ? " has-error" : "")}
+          >
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+            {submitted && !email && (
+              <div className="help-block">Email is required</div>
+            )}
+          </div>
+          <div
+            className={
+              "form-group" + (submitted && !password ? " has-error" : "")
+            }
+          >
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+            {submitted && !password && (
+              <div className="help-block">Password is required</div>
+            )}
+          </div>
+          <div
+            className={
+              "form-group" +
+              (submitted && !password_confirmation ? " has-error" : "")
+            }
+          >
+            <label htmlFor="password">Confirm Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password_confirmation"
+              value={password_confirmation}
+              onChange={handleChange}
+            />
+            {submitted && !password_confirmation && (
+              <div className="help-block">
+                Password Confirmation is required
+              </div>
+            )}
+          </div>
+          <div className="form-group">
+            <Button>Sign Up</Button>
+            {loggingIn && (
+              <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+            )}
+          </div>
+        </form>
+        <Link to="/login">Login</Link>
+      </Container>
     </div>
   );
 }
